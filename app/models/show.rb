c0a4bee67e3dd8ad94bf05b4f0067f1b2b72f86c
Anthony Rurup <tcrurup@gmail.com>
2019-05-22 18:38:48 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
-  def build_network(call_letters:)
+  def build_network(call_letters)
     Network.create(call_letters)
   end
   
