bc6053df03d8dfe2ffcc9341283773494e7c21a0
Anthony Rurup <tcrurup@gmail.com>
2019-05-22 18:38:18 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,10 +2,6 @@ class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
   
-  def build_network(call_letters:)
-    Network.create(call_letters)
-  end
-  
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
